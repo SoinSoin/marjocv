38d07a7fa0f072870de2d5cd44a76896f325f364
SoinSoin <vincent.retore@gmail.com>
2018-01-11 15:04:17 +0100

    marjo cv

---
+++ js/cv.js
@@ -0,0 +1,35 @@
+console.log("bob")
+
+function deroulant() {
+    var x = document.getElementById('comp');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function deroulant1() {
+    var x = document.getElementById('prjt');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function deroulant2() {
+    var x = document.getElementById('form');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function deroulant3() {
+    var x = document.getElementById('exp');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
